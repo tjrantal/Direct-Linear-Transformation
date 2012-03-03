6d4c2eebe218e6777ea9ce01b14d0332632a80d6
Timo Rantalainen <tjrantal@mgail.com>
2012-03-03 19:09:26 +1100

    2D DLT

---
+++ Sample.java
@@ -0,0 +1,40 @@
+/*
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	N.B.  the above text was copied from http://www.gnu.org/licenses/gpl.html
+	unmodified. I have not attached a copy of the GNU license to the source...
+
+    Copyright (C) 2012 Timo Rantalainen
+*/
+/*
+	Direct linear transformation (DLT) sample, written by Timo Rantalainen tjrantal@gmail.com.
+	Depends on Jama library http://math.nist.gov/javanumerics/jama/
+	compile javac Sample.java
+	execute java Sample
+*/
+
+import Jama.*;
+import dlt.*;
+
+public class Sample{
+	 public static void main(String[] args) {
+		double[][] global =  {{0,0}, {29.6, 0}, {0, 21}, {29.6, 21}};  //Global coordinates
+		double[][] calib = {{900,724},{2320, 575},{955, 1711},{ 2610, 1501}};  //Calibration points
+		double[]  digitizedUnknownPoints = {1245.0,919.0};	//Tuntemattoman pisteen digitoidut koordinaatit
+		System.out.println("DLT2D");
+		DLT2D dlt2d = new DLT2D(global,calib);
+		Matrix coordinates = dlt2d.scaleCoordinates(digitizedUnknownPoints);
+		System.out.println("X "+coordinates.get(0,0)+" Y "+coordinates.get(1,0));
+	}
+}
---
+++ dlt/DLT2D.java
@@ -0,0 +1,113 @@
+/*
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	N.B.  the above text was copied from http://www.gnu.org/licenses/gpl.html
+	unmodified. I have not attached a copy of the GNU license to the source...
+
+    Copyright (C) 2012 Timo Rantalainen
+*/
+/*
+	2D DLT (direct linear transformation).
+	Depends on Jama library http://math.nist.gov/javanumerics/jama/
+	Written by Timo Rantalainen tjrantal@gmail.com
+*/
+
+package dlt;
+import Jama.*;
+
+public class DLT2D{
+	Matrix calibrationObjectGlobalCoordinates;
+	Matrix dltCoefficients;
+	public DLT2D(){}
+	public DLT2D(double[][] calibrationObjectGlobalCoordinates){
+		this.calibrationObjectGlobalCoordinates = new Matrix(calibrationObjectGlobalCoordinates);
+	}
+	
+	public DLT2D(double[][] calibrationObjectGlobalCoordinates, double[][] digitizedCalibrationObjectCoordinates){
+		this.calibrationObjectGlobalCoordinates = new Matrix(calibrationObjectGlobalCoordinates);
+		this.dltCoefficients = getDltCoefficients(calibrationObjectGlobalCoordinates,digitizedCalibrationObjectCoordinates);		
+	}
+	
+	public void setCalibrationObjectGlobalCoordinates(double[][] calibrationObjectGlobalCoordinates){
+		this.calibrationObjectGlobalCoordinates = new Matrix(calibrationObjectGlobalCoordinates);
+	}
+	
+	public void setCalibrationObjectGlobalCoordinates(Matrix calibrationObjectGlobalCoordinates){
+		this.calibrationObjectGlobalCoordinates = calibrationObjectGlobalCoordinates;
+	}
+	
+		public Matrix getDltCoefficients(double[][] L){
+		return getDltCoefficients(this.calibrationObjectGlobalCoordinates,L);
+	}
+	
+	public Matrix getDltCoefficients(Matrix calibrationObjectGlobalCoordinates,double[][] L){
+		return getDltCoefficients(calibrationObjectGlobalCoordinates.getArray(),L);
+	}
+	
+	public Matrix getDltCoefficients(double[][] calibrationObjectGlobalCoordinates,double[][] L){
+		double[][] B = new double[2*calibrationObjectGlobalCoordinates.length][8];//Matrix for solving DLT-parameters
+		double[] C = new double[2*L.length]; //Digitized calibrationObject coordinates
+		int monta = 0;
+		for (int j =0;j<L.length;j++){
+			for (int i =0;i<2;i++){
+				C[monta] = L[j][i];
+				++monta;
+			}
+		}
+
+		for (int i= 0;i<calibrationObjectGlobalCoordinates.length;++i){
+		  B[2*i][0]			= calibrationObjectGlobalCoordinates[i][0]; 
+		  B[2*i][1]			= calibrationObjectGlobalCoordinates[i][1]; 
+		  B[2*i][2]  		= 1;
+		  B[2*i][6]		 	=-calibrationObjectGlobalCoordinates[i][0]*L[i][0];
+		  B[2*i][7]			=-calibrationObjectGlobalCoordinates[i][1]*L[i][0];
+		  B[2*i+1][3]		= calibrationObjectGlobalCoordinates[i][0];
+		  B[2*i+1][4]		= calibrationObjectGlobalCoordinates[i][1];
+		  B[2*i+1][5]		= 1;
+		  B[2*i+1][6]		=-calibrationObjectGlobalCoordinates[i][0]*L[i][1];
+		  B[2*i+1][7]		=-calibrationObjectGlobalCoordinates[i][1]*L[i][1];
+		}
+
+		// Solution for the coefficients
+		Matrix A = new Matrix(B);
+		Matrix b = new Matrix(C,C.length);
+		Matrix coefficients = A.solve(b);
+		String kertoimet = "Coefficient";
+		for (int i = 0; i< coefficients.getRowDimension();++i){
+			kertoimet+=" "+i+": "+Math.round(coefficients.get(i,0)*10.0)/10.0; 
+		}
+		System.out.println(kertoimet);
+		return coefficients;	
+	}
+
+	public Matrix scaleCoordinates(double[] coordinates){
+		return scaleCoordinates(this.dltCoefficients,coordinates);
+	}
+	
+	public Matrix scaleCoordinates(Matrix coefficients, double[] coordinates){
+		double[][]	L1 = new double[2][2];
+		double[]		L2 = new double[2];
+		L1[0][0]	= coefficients.get(0,0)-coordinates[0]*coefficients.get(6,0);
+		L1[0][1]	= coefficients.get(1,0)-coordinates[0]*coefficients.get(7,0);
+		L1[1][0]	= coefficients.get(3,0)-coordinates[1]*coefficients.get(6,0);
+		L1[1][1]	= coefficients.get(4,0)-coordinates[1]*coefficients.get(7,0);
+		L2[0]		= coordinates[0]-coefficients.get(2,0);
+		L2[1]		= coordinates[1]-coefficients.get(5,0);
+
+		Matrix l1 = new Matrix(L1);
+		Matrix l2 = new Matrix(L2,L2.length);
+		Matrix result= l1.solve(l2);
+		return result;
+	}
+}
